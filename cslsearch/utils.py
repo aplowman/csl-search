2f02a7b32d64e7a54b74179bc5b4473676967680
APlowman <adam.jp@live.co.uk>
2017-11-30 23:28:13 +0000

    Improve prt utility function

--- cslsearch/utils.py
+++ cslsearch/utils.py
@@ -3,19 +3,38 @@
 import numpy as np
 
 
-def prt(obj, name):
+def prt(obj, name, slc=None):
     """
     Print an object, with it's size and dtype if it's an ndarray.
+
+    Helpful for debugging.
 
     Parameters
     ----------
     obj : object
         Object to print.
     name : str
-        Name of object to print. 
+        Name of object to print.
+    slc : tuple of slice object, optional
+        If not None, only print the sub array defined by the slice.
     """
 
     if isinstance(obj, np.ndarray):
-        print('{} {} {}: \n{}\n'.format(name, obj.shape, obj.dtype, obj))
+
+        if slc is not None:
+            slc_str = ', '.join(['{}:{}:{}'.format(
+                s.start or '', s.stop or '', s.step or '') for s in slc])
+            slc_str = '[' + slc_str + ']'
+        else:
+            slc_str = ''
+
+        fmt_str = '{} {} {} {}: \n{}\n'
+        fmts = (name, obj.shape, slc_str,
+                obj.dtype, obj[slc] if slc is not None else obj)
+
     else:
-        print('{}: \n{}\n'.format(name, obj))
+
+        fmt_str = '{} ({}): \n{}\n'
+        fmts = (name, type(obj), obj)
+
+    print(fmt_str.format(*fmts))
