145e71eba92864cac95e83eb862ee4fdf2f1f309
APlowman <adam.jp@live.co.uk>
2017-12-03 12:47:25 +0000

    Find rotation angles about each valid rotation axis within search size

--- cslsearch/csl.py
+++ cslsearch/csl.py
@@ -68,7 +68,9 @@ def main(search_size):
 
     # Find rotation axes for each trial pair
     trial_pair = trial[:, trial_pair_idx].transpose(2, 0, 1)
-    trial_pair_rotax = np.cross(*trial_pair, axis=0)
+    tp_rotax = np.cross(*trial_pair, axis=0)
+    tp_rotax_u = tp_rotax / np.linalg.norm(tp_rotax, axis=0)
+    tp_dot = np.einsum('ij,ij->j', *trial_pair)
 
     prt(trial_lat, 'trial_lat', t1col)
     prt(trial, 'trial', t1col)
@@ -77,7 +79,51 @@ def main(search_size):
     prt(trial_mag_uq, 'trial_mag_uq')
     prt(trial_pair_idx, 'trial_pair_idx', t1row)
     prt(trial_pair, 'trial_pair', t1col_3d)
-    prt(trial_pair_rotax, 'trial_pair_rotax', t1col)
+    prt(tp_rotax, 'tp_rotax', t1col)
+    prt(tp_rotax_u, 'tp_rotax_u', t1col)
+    prt(tp_dot, 'tp_dot', t1)
+
+    # Find which trial pair rotation axes are vectors within the search size.
+    # `rotax_idx`: keys index `trial`, values are lists which index `trial_pair`
+    rotax_idx = vectors.get_parallel_idx(trial, tp_rotax)
+
+    print('Found {} rotation axes within search size.'.format(len(rotax_idx)))
+    prt(rotax_idx, 'rotax_idx')
+
+    # Loop through each rotation axis:
+    for rotax_idx_i, trial_pair_idx_i in rotax_idx.items():
+
+        prt(rotax_idx_i, 'rotax_idx_i')
+        prt(trial_pair_idx_i, 'trial_pair_idx_i')
+
+        # Find rotation angle for each vector pair about this axis:
+        rotax_i = trial[:, rotax_idx_i][:, np.newaxis]
+        rotax_unit_i = rotax_i / np.linalg.norm(rotax_i)
+        trial_pair_i = trial_pair[:, :, trial_pair_idx_i]  # Not needed
+
+        prt(rotax_i, 'rotax_i')
+        prt(rotax_unit_i, 'rotax_unit_i')
+        prt(trial_pair_i, 'trial_pair_i')
+
+        # Get cross and dot products (previously computed):
+        tp_i_cross = tp_rotax[:, trial_pair_idx_i]
+        tp_i_dot = tp_dot[trial_pair_idx_i]
+
+        prt(tp_i_cross, 'tp_i_cross')
+        prt(tp_i_dot, 'tp_i_dot')
+
+        # Get (signed) rotation angles about this axis:
+        atan_arg1 = np.einsum('ij,ik->k', rotax_unit_i, tp_i_cross)
+        atan_arg2 = tp_i_dot
+        theta_i = np.arctan2(atan_arg1, atan_arg2)
+        theta_i_deg = np.rad2deg(theta_i)
+
+        prt(atan_arg1, 'atan_arg1')
+        prt(atan_arg2, 'atan_arg2')
+        prt(theta_i, 'theta_i')
+        prt(theta_i_deg, 'theta_i_deg')
+
+        break
 
 
 if __name__ == '__main__':
